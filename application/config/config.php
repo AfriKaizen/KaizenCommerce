80d98a4e930c13189f068ceccd50651b96610e3f
Steven Dumbura <stevedumbura@gmail.com>
2015-07-13 15:13:44 +0200

    Kaizen Commerce V 0.1.0

--- application/config/config.php
+++ application/config/config.php
@@ -279,12 +279,12 @@ $config['encryption_key'] = 'KaizenCommerce';
 |
 */
 $config['sess_driver'] = 'database';
-$config['sess_cookie_name'] = 'ci_session';
+$config['sess_cookie_name'] = 'kaizencookie'; 
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 0;
-$config['sess_regenerate_destroy'] = FALSE;;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
